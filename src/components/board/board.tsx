38d168ef4209242a38557216c85235a36621b1f8
Antoine Corre <antoine.corre@digitalfactory.dev>
2020-04-01 07:31:56 +0200

    up

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -36,14 +36,13 @@ export class Board {
   @State() lineSpace = this.getLineSpace(this.width, this.size, this.padding)
   @State() lines = this.getLines();
   @State() coordMarkers = this.getCoordMarkers();
-  @State() linesPath = [this.innerPath, ...this.lines].join(' ');
   @Watch('size')
   onSizeChange(newSize: number) {
     console.log({newSize});
     this.lineSpace = this.getLineSpace(this.width, this.size, this.padding)
     this.lines = this.getLines();
     this.coordMarkers = this.getCoordMarkers();
-    this.linesPath = [this.innerPath, ...this.lines].join(' ');
+
   }
 
   handleOver(e: MouseEvent) {
@@ -130,7 +129,7 @@ export class Board {
       x: this.getPos(x),
       y: this.getPos(y)
     }));
-
+    const linesPath = [this.innerPath, ...this.lines].join(' ');
     return (
       <svg
       class="svgboard"
@@ -243,7 +242,7 @@ export class Board {
         {this.renderStoneSymbol(this.lineSpace, WHITE)}
         {markers.length && this.renderMarkersSymbol(this.lineSpace) }
         <path class="woodboard" data-textured={this.options.style?.texture} d={this.outerPath}></path>
-        <path class="lines" d={this.linesPath}></path>
+        <path class="lines" d={linesPath}></path>
         {hoshis.map(({x, y}) => <use xlinkHref="#hoshi" x={x} y={y}/>)}
         <g class="coords">{this.coordMarkers.map(({m, x, y}) => <text x={x} y={y}>{m}</text>)}</g>
         <g class="stones" filter="url(#MyFilter)">
